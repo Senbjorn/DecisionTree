db360009be15374369f88d0bc4e32a64d87cff27
Semyon Glushkov <semen.glushkov.m@gmail.com>
2019-04-15 21:29:08 +0300

    It works! - decision_tree fit, predict - plots

--- source/decision_tree.py
+++ source/decision_tree.py
@@ -1,12 +1,115 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import time
 from simple_tree import *
+from matplotlib.colors import ListedColormap
+
+# caret R language (ml package)
+# meltdown and specter
+# Titanic machine learning kaggle
+# light gbm
 
 
-class DecisionTree:
+def gini(data_y, frequencies):
+    return np.sum(frequencies[data_y] * (1 - frequencies[data_y]))
+
+
+class DecisionTreeClassifier:
     def __init__(self):
-        pass
-
+        self.data = None
+        self.data_index = None
+        self.frequencies = None
+        self.classes = None
+        self.class_indices = None
+        self.n_classes = None
+        self.n_features = None
+        self.n_samples = None
+        self.root = None
 
     def fit(self, data):
-        root = Node()
+        self.data = data
+        self.n_features = len(data[0][0])
+        self.n_samples = len(data[1])
+        self.classes = np.unique(data[1], return_counts=True)
+        self.n_classes = len(self.classes[0])
+        self.class_indices = {v: i for i, v in enumerate(self.classes[0])}
+        self.frequencies = self.classes[1] / np.sum(self.classes[1])
+        self.data_index = (data[0], np.array([self.class_indices[y] for y in data[1]]))
+        self.root = BinaryFilterNode()
+        self.fit_gini([i for i in range(self.n_samples)], self.root)
 
+    def fit_gini(self, pos, node, min_count=1):
+        m = len(pos)
+        min_j = None
+        min_i = None
+        min_value = 1e+300
+        for j in range(self.n_features):
+            pos_j = sorted(pos, key=lambda x: self.data_index[0][x, j])
+            for i in range(m - 1):
+                value = ((i + 1) / m * gini(self.data_index[1][pos_j[:i+1]], self.frequencies) +
+                        (m - i - 1) / m * gini(self.data_index[1][pos_j[i+1:]], self.frequencies))
+                if value <= min_value:
+                    min_value = value
+                    min_j = j
+                    min_i = i
+        pos_j = sorted(pos, key=lambda x: self.data_index[0][x, min_j])
+        left_pos = list(pos_j[:min_i + 1])
+        right_pos = list(pos_j[min_i + 1:])
+        left_data = self.data_index[1][left_pos]
+        right_data = self.data_index[1][right_pos]
+        node.data = (self.data_index[0][pos_j[min_i], min_j], min_j)
+        left_node = BinaryFilterNode()
+        right_node = BinaryFilterNode()
+        if min_i + 1 <= min_count:
+            node.add_successor(
+                LeafNode([len(left_data[left_data == i]) / len(left_pos) for i in range(self.n_classes)]))
+        else:
+            # print("l:", left_pos)
+            node.add_successor(left_node)
+            self.fit_gini(left_pos, left_node, min_count=min_count)
+        if m - (min_i + 1) <= min_count:
+            node.add_successor(
+                LeafNode([len(right_data[right_data == i]) / len(right_pos) for i in range(self.n_classes)]))
+        else:
+            # print("r:", right_pos)
+            node.add_successor(right_node)
+            self.fit_gini(right_pos, right_node, min_count=min_count)
+
+    def predict(self, x):
+        node = self.root
+        while not isinstance(node, LeafNode):
+            a = node.data[0]
+            j = node.data[1]
+            if x[j] <= a:
+                node = node.successors[0]
+            else:
+                node = node.successors[1]
+        return node.value
+
+
+def get_meshgrid(data, step=0.01, border=2.0):
+    min_x, max_x = np.min(data[0][:, 0]) - border, np.max(data[0][:, 0]) + border
+    min_y, max_y = np.min(data[0][:, 1]) - border, np.max(data[0][:, 1]) + border
+    return np.meshgrid(np.arange(min_x, max_x, step), np.arange(min_y, max_y, step))
+
+
+if __name__ == "__main__":
+    from sklearn import datasets
+    start_time = time.time()
+    d = datasets.make_classification(100, n_features=2, n_informative=2, n_redundant=0, n_classes=3, n_clusters_per_class=1)
+    dtc = DecisionTreeClassifier()
+    dtc.fit(d)
+    xx, yy = get_meshgrid(d)
+    p_grid = np.zeros(xx.shape)
+    for i in range(xx.shape[0]):
+        for j in range(xx.shape[1]):
+            p_grid[i, j] = dtc.classes[0][np.argmax(dtc.predict([xx[i, j], yy[i, j]]))]
+    end_time = time.time() - start_time
+    print("elaped:", end_time)
+    colors = ListedColormap(["red", "blue", "yellow"])
+    colors_light = ListedColormap(['lightcoral', 'lightblue', 'lightyellow'])
+    fig, ax = plt.subplots()
+    ax.pcolormesh(xx, yy, p_grid, cmap=colors_light)
+    ax.scatter(list(map(lambda x: x[0], d[0])), list(map(lambda x: x[1], d[0])),
+               c=d[1], cmap=colors, s=100)
+    plt.show()
--- source/simple_tree.py
+++ source/simple_tree.py
@@ -8,19 +8,31 @@ class Node:
 
     def add_successor(self, s):
         self.successors.append(s)
+        s.parent = self
 
     def insert_successor(self, s, i):
         self.successors.insert(i, s)
+        s.parent = self
 
     def remove_successor(self, i):
-        self.successors.pop(i)
+        s = self.successors.pop(i)
+        s.parent = None
 
 
 class BinaryFilterNode(Node):
-    def __init__(self, a, j, parent=None, successors=None):
+    def __init__(self, a=None, j=None, parent=None, successors=None):
         super().__init__(parent, successors)
         self.data = (a, j)
 
     def filter_data(self, data):
-        left = None
-        right = None
+        left_cond = data[0][self.data[1]] <= self.data[0]
+        right_cond = data[0][self.data[1]] > self.data[0]
+        left = (data[0][left_cond], data[1][left_cond])
+        right = (data[0][right_cond], data[1][right_cond])
+        return left, right
+
+
+class LeafNode(Node):
+    def __init__(self, value, parent=None, successors=None):
+        super().__init__(parent, successors)
+        self.value = value
